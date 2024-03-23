901a48fa2e707a9a7860f860c60f3fb38e23b321
JTrippi-W <jtrip578968@gmail.com>
2024-03-23 18:58:50 -0400

    Update App component with the Header component to be rendered above all routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Posts from './features/posts/Posts';
+import Post from './features/post/Post';
+// import SearchResults from './features/searchResults/SearchResults';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/r/:subreddit" element={<Posts />} />
+        {/* <Route path="/search/:searchTerm" element={<SearchResults />} /> */}
+        <Route path="/post/:encodedPermalink" element={<Post />} />
+      </Routes>
+    </Router>
   );
 }
 
