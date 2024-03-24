d47c13e98ed1d7a359a01e6f8b3bafbde1c3e482
JTrippi-W <jtrip578968@gmail.com>
2024-03-24 09:23:46 -0400

    Refactor to resolve linter errors

--- src/utils/useRetryCountdown.js
+++ src/utils/useRetryCountdown.js
@@ -1,32 +1,34 @@
 import { useEffect, useState } from 'react';
 
 const useRetryCountdown = (startCountdown, initialCountdown = 5) => {
-    const [countdown, setCountdown] = useState(initialCountdown);
-    const [progress, setProgress] = useState(100);
-    const [retryAvailable, setRetryAvailable] = useState(false);
+  const [countdown, setCountdown] = useState(initialCountdown);
+  const [progress, setProgress] = useState(100);
+  const [retryAvailable, setRetryAvailable] = useState(false);
 
-    useEffect(() => {
-        if (startCountdown) {
-            setRetryAvailable(false);
-            setCountdown(initialCountdown);
-            setProgress(100);
+  useEffect(() => {
+    if (startCountdown) {
+      setRetryAvailable(false);
+      setCountdown(initialCountdown);
+      setProgress(100);
 
-            const interval = setInterval(() => {
-                setCountdown((currentCount) => {
-                    const nextCount = currentCount - 1;
-                    if (nextCount <= 0) {
-                        clearInterval(interval);
-                        setRetryAvailable(true);
-                        return 0;
-                    }
-                    setProgress((nextCount / initialCountdown) * 100);
-                    return nextCount;
-                });
-            }, 1000);
+      const interval = setInterval(() => {
+        setCountdown((currentCount) => {
+          const nextCount = currentCount - 1;
+          if (nextCount <= 0) {
+            clearInterval(interval);
+            setRetryAvailable(true);
+            return 0;
+          }
+          setProgress((nextCount / initialCountdown) * 100);
+          return nextCount;
+        });
+      }, 1000);
 
-            return () => clearInterval(interval);
-        }
-    }, [startCountdown, initialCountdown]);
+      return () => clearInterval(interval);
+    }
+  }, [startCountdown, initialCountdown]);
 
-    return { countdown, retryAvailable, setRetryAvailable, progress };
+  return { countdown, retryAvailable, setRetryAvailable, progress };
 };
+
+export default useRetryCountdown;
