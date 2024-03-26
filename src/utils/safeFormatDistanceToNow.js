4c4368d73a7cbc2bd6d173da5bf92ed62534bc4d
JTrippi-W <jtrip578968@gmail.com>
2024-03-26 13:14:56 -0400

    Update the Posts component to use date-fns instead of the safeFormatToNow helper function. Remove safeFormatToNow as it is no longer used.

--- src/utils/safeFormatDistanceToNow.js
+++
@@ -1,8 +0,0 @@
-import { formatDistanceToNow } from 'date-fns';
-
-export function safeFormatDistanceToNow(timestamp) {
-  if (timestamp == null) return 'Unkown time';
-  const date = new Date(timestamp * 1000);
-  if (isNaN(date.getTime())) return 'Invalid date';
-  return formatDistanceToNow(date);
-}
