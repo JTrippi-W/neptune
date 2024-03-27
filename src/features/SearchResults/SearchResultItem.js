4e6295bb56de7a8d45cf4c1d381617ea4da4daa1
JTrippi-W <jtrip578968@gmail.com>
2024-03-26 22:33:30 -0400

    Update the swtich statement cases to use curly brackets instead of implicit return to make the code more readable

--- src/features/SearchResults/SearchResultItem.js
+++ src/features/SearchResults/SearchResultItem.js
@@ -6,7 +6,8 @@ import RenderThumbnail from '../../utils/RenderThumbnail';
 // The search endpoint provides more types than the subreddit posts endpoint
 const SearchResultItem = ({ item }) => {
   switch (item.kind) {
-    case 't3': // link/post
+    case 't3': {
+      // link/post
       return (
         <li key={item.data.id}>
           <h3>{item.data.title}</h3>
@@ -21,7 +22,9 @@ const SearchResultItem = ({ item }) => {
           </p>
         </li>
       );
-    case 't1': // comment
+    }
+    case 't1': {
+      // comment
       return (
         <li key={item.data.id}>
           <p>{item.data.body}</p>
@@ -30,17 +33,31 @@ const SearchResultItem = ({ item }) => {
           </p>
         </li>
       );
-    case 't5': // subreddit
+    }
+    case 't5': {
+      // subreddit
+      const subredditImage = item.data.header_img;
       return (
         <li key={item.data.id}>
+          {subredditImage && (
+            <img
+              src={subredditImage}
+              alt={item.data.display_name}
+              style={{ maxWidth: '100px', maxHeight: '100px' }}
+            />
+          )}
           <h3>{item.data.display_name}</h3>
           <p>{item.data.public_description}</p>
-          <p>{item.data.subscribers} subscribers</p>
+          <p>
+            {item.data.subscribers.toLocaleString()} subscribers | {item.data.accounts_active}{' '}
+            active now
+          </p>
           <p>
             <Link to={`/r/${item.data.display_name}`}>Visit</Link>
           </p>
         </li>
       );
+    }
     default:
       return null;
   }
@@ -65,8 +82,10 @@ SearchResultItem.propTypes = {
         // subreddit specific
         display_name: PropTypes.string,
         public_description: PropTypes.string,
-        subscribers: PropTypes.number
-      })
+        subscribers: PropTypes.number,
+        header_img: PropTypes.string,
+        accounts_active: PropTypes.number
+      }).isRequired
     ]).isRequired
   }).isRequired
 };
