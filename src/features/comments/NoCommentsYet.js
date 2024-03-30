3dc5afab77ac7fcedfa12810d42f2ae348ba046a
JTrippi-W <jtrip578968@gmail.com>
2024-03-30 00:16:28 -0400

    Update NoCommentsYet component to align with style rules.

--- src/features/comments/NoCommentsYet.js
+++ src/features/comments/NoCommentsYet.js
@@ -1,10 +1,10 @@
 const NoCommentsYet = () => {
-    return (
-        <div>
-            <h4>No Comments Yet</h4>
-            <p>Be the first to share.</p>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h4>No Comments Yet</h4>
+      <p>Be the first to share.</p>
+    </div>
+  );
+};
 
 export default NoCommentsYet;
