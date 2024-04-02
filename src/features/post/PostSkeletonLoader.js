0504c25c30c5f1486bdf0312fe84a7e635f3660c
JTrippi-W <jtrip578968@gmail.com>
2024-04-02 02:12:56 -0400

    Refactor the PostSkeletonLoader component to better represent the generic structure of a post

--- src/features/post/PostSkeletonLoader.js
+++ src/features/post/PostSkeletonLoader.js
@@ -2,10 +2,12 @@ import styles from './PostSkeletonLoader.module.css';
 
 const PostSkeletonLoader = () => {
   return (
-    <div className={styles.postSkeleton}>
+    <div className={styles.postSkeleton} aria-hidden="true">
       <div className={styles.skeletonTitle}></div>
-      <div className={styles.skeletonText}></div>
-      <div className={styles.skeletonText}></div>
+      <div className={styles.mediaPlaceholder}></div>
+      {[...Array(3)].map((_, index) => (
+        <div key={index} className={styles.skeletonText}></div>
+      ))}
       <div className={styles.skeletonFooter}></div>
     </div>
   );
