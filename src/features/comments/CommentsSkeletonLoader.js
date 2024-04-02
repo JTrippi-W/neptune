1204f00972b6a4296ba772ab3023679779422535
JTrippi-W <jtrip578968@gmail.com>
2024-04-02 01:51:35 -0400

    Refactor the CommentsSkeletonLoader to be more consistent and implement random rendering of nested replies to mimic the structure of real comments

--- src/features/comments/CommentsSkeletonLoader.js
+++ src/features/comments/CommentsSkeletonLoader.js
@@ -1,11 +1,22 @@
 import styles from './CommentsSkeletonLoader.module.css';
 
 const CommentsSkeletonLoader = () => {
+  const indentClasses = [styles.level0, styles.level1, styles.level2];
+
   return (
-    <div className={styles.commentsSkeleton}>
+    <div className={styles.commentsSkeleton} aria-hidden="true">
       <hr />
       {Array.from({ length: 15 }).map((_, index) => (
-        <div key={index} className={styles.skeletonComment}></div>
+        <div key={index} className={styles.skeletonComment}>
+          {Array.from({ length: 10 }).map((_, index) => (
+            <div
+              key={index}
+              className={`${styles.skeletonComment} ${indentClasses[index % indentClasses.length]}`}>
+              <div className={styles.skeletonAuthor}></div>
+              <div className={styles.skeletonText}></div>
+            </div>
+          ))}
+        </div>
       ))}
     </div>
   );
